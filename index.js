cf5726ac3d9cab3376ed6aff28cee19e099debc5
Anuja Ulpathakumbura <anuja.ulp@gmail.com>
2020-11-22 18:21:51 +0800

    setup structure from routes to model

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postsRoutes from './routes/posts.js';
+
 const app = express(); // initialize the app
 
+app.use('/posts', postsRoutes);
 // setting up bodyParser to properly send the requests
 // limit is set to 30MB because we need to upload images
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors());
 
-const CONNECTION_URL = '';
+const CONNECTION_URL = 'mongodb+srv://anuja:testuser1989@momories-cluster.mgpsp.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 // DB connection
---
+++ models/postMessage.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+// schema is an object that allows you to define the shape and content of documents 
+// and embedded documents in a collection
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    }
+})
+
+const PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage;
